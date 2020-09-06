835fef4e528f2d01b4901ae31a3773354ef4715d
apul8377 <apulvers@my.chemeketa.edu>
2020-09-06 03:18:46 -0700

    in newKegControl added currentlyVisibleState to state, added addKegButton to render

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import NewKegControl from "./AddKeg.js";
 function App() {
   return (
     <React.Fragment>
       <KegList />
+      <NewKegControl />
     </React.Fragment>
   );
 }
--- src/components/NewKegControl.js
+++ src/components/NewKegControl.js
@@ -1,15 +1,31 @@
 import React from "react";
-import NewKegForm from "./AddKeg";
-import KegList from "./KegList";
+import NewKegForm from "./AddKeg.js";
+import KegList from "./KegList.js";
 
 class KegControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false,
+    };
   }
 
   render() {
-    return <React.Fragment></React.Fragment>;
+    let currentlyVisibleState = null;
+    let addKegButton = null;
+
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm />;
+    } else {
+      currentlyVisibleState = <KegList />;
+      addKegButton = <button onClick={this.handleClick}>Add new keg!</button>;
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        {addKegButton}
+      </React.Fragment>
+    );
   }
 }
 
